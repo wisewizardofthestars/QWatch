ce9d449208d121c05d0510b951dec1fb3d399bd8
powy-e <edfnazario@gmail.com>
2023-04-15 15:37:37 +0100

    Added prisma and backend to login and create users

---
+++ database_test/create_user.js
@@ -0,0 +1,17 @@
+import {PrismaClient} from '@prisma/client'
+
+
+export default async function create_user (email, name, password) {
+
+    const db = new PrismaClient()
+
+    db.user.create({
+        data: {
+            email: email,
+            name: name,
+            password: password,        
+        }
+    }).catch((e) => { console.log(e); return "ERROR: Can't create user"});
+
+    return true;
+}
